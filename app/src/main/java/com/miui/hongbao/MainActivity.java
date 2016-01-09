3a05a37d619629a9b5ad1c036ceee4df170c8283
ray26 <romerxzh@gmail.com>
2016-01-09 23:13:28 +0800

    update ui and sdk

--- app/src/main/java/com/miui/hongbao/MainActivity.java
+++ app/src/main/java/com/miui/hongbao/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends Activity {
     }
 
     public void openGithub(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/geeeeeeeeek/WeChatLuckyMoney"));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/ray26/WeChatLuckyMoney.git"));
         startActivity(browserIntent);
     }
 }
